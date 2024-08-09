57ccda18963bd683b92b2a69f8a0b2c344aabdfd
oksuliini <setoinenosku@gmail.com>
2024-08-09 12:14:59 +0200

    sovelluksen löydetty kopio

---
+++ TodoListApp/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace TodoListApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.taskTextBox = new System.Windows.Forms.TextBox();
+            this.addTaskButton = new System.Windows.Forms.Button();
+            this.taskListBox = new System.Windows.Forms.ListBox();
+            this.removeTaskButton = new System.Windows.Forms.Button();
+            this.markAsDoneButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.priorityComboBox = new System.Windows.Forms.ComboBox();
+            this.deadlinePicker = new System.Windows.Forms.DateTimePicker();
+            this.reminderTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // taskTextBox
+            // 
+            this.taskTextBox.Location = new System.Drawing.Point(40, 35);
+            this.taskTextBox.Name = "taskTextBox";
+            this.taskTextBox.Size = new System.Drawing.Size(200, 20);
+            this.taskTextBox.TabIndex = 0;
+            // 
+            // addTaskButton
+            // 
+            this.addTaskButton.Location = new System.Drawing.Point(156, 114);
+            this.addTaskButton.Name = "addTaskButton";
+            this.addTaskButton.Size = new System.Drawing.Size(84, 23);
+            this.addTaskButton.TabIndex = 1;
+            this.addTaskButton.Text = "Add Task";
+            this.addTaskButton.UseVisualStyleBackColor = true;
+            this.addTaskButton.Click += new System.EventHandler(this.addTaskButton_Click);
+            // 
+            // taskListBox
+            // 
+            this.taskListBox.FormattingEnabled = true;
+            this.taskListBox.Location = new System.Drawing.Point(259, 35);
+            this.taskListBox.Name = "taskListBox";
+            this.taskListBox.Size = new System.Drawing.Size(269, 186);
+            this.taskListBox.TabIndex = 2;
+            // 
+            // removeTaskButton
+            // 
+            this.removeTaskButton.Location = new System.Drawing.Point(156, 155);
+            this.removeTaskButton.Name = "removeTaskButton";
+            this.removeTaskButton.Size = new System.Drawing.Size(84, 23);
+            this.removeTaskButton.TabIndex = 3;
+            this.removeTaskButton.Text = "Remove Task";
+            this.removeTaskButton.UseVisualStyleBackColor = true;
+            this.removeTaskButton.Click += new System.EventHandler(this.removeTaskButton_Click);
+            // 
+            // markAsDoneButton
+            // 
+            this.markAsDoneButton.Location = new System.Drawing.Point(155, 198);
+            this.markAsDoneButton.Name = "markAsDoneButton";
+            this.markAsDoneButton.Size = new System.Drawing.Size(85, 23);
+            this.markAsDoneButton.TabIndex = 4;
+            this.markAsDoneButton.Text = "Mark as Done";
+            this.markAsDoneButton.UseVisualStyleBackColor = true;
+            this.markAsDoneButton.Click += new System.EventHandler(this.markAsDoneButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(259, 227);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(349, 227);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(85, 23);
+            this.loadButton.TabIndex = 6;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // priorityComboBox
+            // 
+            this.priorityComboBox.FormattingEnabled = true;
+            this.priorityComboBox.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High"});
+            this.priorityComboBox.Location = new System.Drawing.Point(40, 61);
+            this.priorityComboBox.Name = "priorityComboBox";
+            this.priorityComboBox.Size = new System.Drawing.Size(200, 21);
+            this.priorityComboBox.TabIndex = 7;
+            this.priorityComboBox.Text = "Priority:";
+            // 
+            // deadlinePicker
+            // 
+            this.deadlinePicker.Location = new System.Drawing.Point(40, 88);
+            this.deadlinePicker.Name = "deadlinePicker";
+            this.deadlinePicker.Size = new System.Drawing.Size(200, 20);
+            this.deadlinePicker.TabIndex = 8;
+            // 
+            // reminderTimer
+            // 
+            this.reminderTimer.Interval = 86400000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 361);
+            this.Controls.Add(this.deadlinePicker);
+            this.Controls.Add(this.priorityComboBox);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.markAsDoneButton);
+            this.Controls.Add(this.removeTaskButton);
+            this.Controls.Add(this.taskListBox);
+            this.Controls.Add(this.addTaskButton);
+            this.Controls.Add(this.taskTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox taskTextBox;
+        private System.Windows.Forms.Button addTaskButton;
+        private System.Windows.Forms.ListBox taskListBox;
+        private System.Windows.Forms.Button removeTaskButton;
+        private System.Windows.Forms.Button markAsDoneButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.ComboBox priorityComboBox;
+        private System.Windows.Forms.DateTimePicker deadlinePicker;
+        private System.Windows.Forms.Timer reminderTimer;
+    }
+}
+
---
+++ TodoListApp/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace TodoListApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTaskListBox();
+            reminderTimer.Start();
+        }
+
+        private void InitializeTaskListBox()
+        {
+            taskListBox.DrawMode = DrawMode.OwnerDrawFixed; // Asetetaan piirtotila omistajan piirtämäksi kiinteäksi
+            taskListBox.ItemHeight = 20; // Asetetaan ListBox-kohteiden korkeus
+            taskListBox.DrawItem += taskListBox_DrawItem; // Kytketään DrawItem-tapahtuma käsittelijään
+        }
+
+        private void addTaskButton_Click(object sender, EventArgs e)
+        {
+            string task = taskTextBox.Text.Trim();
+            string priority = priorityComboBox.SelectedItem?.ToString() ?? "Low";
+            string deadline = deadlinePicker.Value.ToShortDateString();
+            if (!string.IsNullOrEmpty(task))
+            {
+                var coloredItem = new ColoredListBoxItem
+                {
+                    Text = $"{task} - Priority: {priority} - Deadline: {deadline}"
+                };
+                SetTaskColor(coloredItem, false); // Aseta väri vain lisättyyn tehtävään
+                taskListBox.Items.Add(coloredItem);
+                SortTasks();
+                taskTextBox.Clear();
+                priorityComboBox.SelectedIndex = -1;
+                deadlinePicker.Value = DateTime.Now;
+            }
+        }
+
+        private void removeTaskButton_Click(object sender, EventArgs e)
+        {
+            if (taskListBox.SelectedItem != null)
+            {
+                var selectedItem = (ColoredListBoxItem)taskListBox.SelectedItem;
+                taskListBox.Items.Remove(selectedItem);
+                RemoveTaskFromFile(selectedItem.Text); // Poistetaan tehtävä myös tiedostosta
+            }
+        }
+
+        private void RemoveTaskFromFile(string taskToRemove)
+        {
+            string filePath = "tasks.txt";
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath).ToList();
+                lines.RemoveAll(line => line.Trim() == taskToRemove.Trim());
+                File.WriteAllLines(filePath, lines);
+            }
+        }
+
+        private void markAsDoneButton_Click(object sender, EventArgs e)
+        {
+            if (taskListBox.SelectedItem != null)
+            {
+                var selectedColoredItem = (ColoredListBoxItem)taskListBox.SelectedItem;
+                var originalTaskText = selectedColoredItem.Text.Split('-')[0].Trim();
+                selectedColoredItem.Text = $"{originalTaskText} - Completed on: {DateTime.Now.ToShortDateString()}";
+                SetTaskColor(selectedColoredItem, true); // Aseta väri merkitylle tehtävälle
+                SortTasks();
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("tasks.txt"))
+            {
+                foreach (ColoredListBoxItem item in taskListBox.Items)
+                {
+                    writer.WriteLine(item.Text);
+                }
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("tasks.txt"))
+            {
+                taskListBox.Items.Clear();
+                using (StreamReader reader = new StreamReader("tasks.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var coloredItem = new ColoredListBoxItem
+                        {
+                            Text = line
+                        };
+                        SetTaskColor(coloredItem, false); // Aseta väri vain ladattuun tehtävään
+                        taskListBox.Items.Add(coloredItem);
+                    }
+                }
+                SortTasks();
+            }
+            else
+            {
+                MessageBox.Show("No saved tasks found.");
+            }
+        }
+
+        private void reminderTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (ColoredListBoxItem item in taskListBox.Items)
+            {
+                string task = item.Text;
+                if (task.Contains("Deadline:"))
+                {
+                    string[] parts = task.Split('-');
+                    string deadlinePart = parts.FirstOrDefault(p => p.Contains("Deadline:"))?.Trim();
+                    if (deadlinePart != null)
+                    {
+                        DateTime deadline = DateTime.Parse(deadlinePart.Replace("Deadline: ", ""));
+                        if (deadline.Date == DateTime.Now.Date)
+                        {
+                            MessageBox.Show($"Reminder: '{parts[0].Trim()}' is due today!", "Reminder");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void SetTaskColor(ColoredListBoxItem item, bool isDone)
+        {
+            if (isDone)
+            {
+                item.Color = Color.Green; // Vihreä väri, kun tehtävä on tehty
+            }
+            else if (item.Text.Contains("High"))
+            {
+                item.Color = Color.Red; // Punainen väri, kun tehtävän kiireisyys on korkea
+            }
+            else if (item.Text.Contains("Medium"))
+            {
+                item.Color = Color.Orange; // Oranssi väri, kun tehtävän kiireisyys on keskitaso
+            }
+            else if (item.Text.Contains("Low"))
+            {
+                item.Color = Color.LightBlue; // Vaaleansininen väri, kun tehtävän kiireisyys on matala
+            }
+            else
+            {
+                item.Color = Color.Black; // Oletusväri muille tehtäville
+            }
+        }
+
+        private void SortTasks()
+        {
+            var tasks = new List<ColoredListBoxItem>();
+            var doneTasks = new List<ColoredListBoxItem>();
+
+            foreach (ColoredListBoxItem item in taskListBox.Items)
+            {
+                if (item.Text.Contains("Completed on:"))
+                {
+                    doneTasks.Add(item);
+                }
+                else
+                {
+                    tasks.Add(item);
+                }
+            }
+
+            taskListBox.Items.Clear();
+
+            foreach (var task in tasks)
+            {
+                taskListBox.Items.Add(task);
+            }
+
+            foreach (var doneTask in doneTasks)
+            {
+                taskListBox.Items.Add(doneTask);
+            }
+        }
+
+        private void taskListBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            var listBox = sender as ListBox;
+            if (listBox != null && e.Index >= 0 && e.Index < listBox.Items.Count)
+            {
+                var item = listBox.Items[e.Index] as ColoredListBoxItem;
+                if (item != null)
+                {
+                    e.Graphics.DrawString(item.Text, e.Font, new SolidBrush(item.Color), e.Bounds);
+                }
+                else
+                {
+                    e.Graphics.DrawString(listBox.Items[e.Index].ToString(), e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+                }
+            }
+            e.DrawFocusRectangle();
+        }
+    }
+
+    public class ColoredListBoxItem
+    {
+        public string Text { get; set; }
+        public Color Color { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
